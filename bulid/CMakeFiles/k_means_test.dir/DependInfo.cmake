3527e73163b3dbabbcdb04db8cf31107bcdcfdb6
konanrobot <konanrobot@163.com>
2015-06-28 15:34:20 +0800

    yel

---
+++ bulid/CMakeFiles/k_means_test.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/exbot/Documents/k-means-master/k-means.cpp" "/home/exbot/Documents/k-means-master/bulid/CMakeFiles/k_means_test.dir/k-means.cpp.o"
+  "/home/exbot/Documents/k-means-master/main.cpp" "/home/exbot/Documents/k-means-master/bulid/CMakeFiles/k_means_test.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ bulid/src/CMakeFiles/k_means_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/k_means_test.dir/main.cpp.o"
+  "CMakeFiles/k_means_test.dir/k-means.cpp.o"
+  "k_means_test.pdb"
+  "k_means_test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/k_means_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
